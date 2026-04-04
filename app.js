096ba4ce56604c23475746533fc33b02ab6f4113
Oğuz Kağan ARISOY <177668475+oguzkgn@users.noreply.github.com>
2026-04-05 00:32:54 +0300

    MongoDB Atlas veritabanı bağlantısı başarıyla kuruldu

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-// Express uygulamasını başlatıyoruz
 const app = express();
 
-// Gelen JSON verilerini okuyabilmek ve dış bağlantılara izin vermek için ayarlar
 app.use(express.json());
 app.use(cors());
 
-// Ana sayfaya girildiğinde çalışacak basit bir test mesajı
+// MongoDB bağlantı linkin
+const dbURI = "mongodb+srv://oguz:KagAn32@cluster0.6bfboko.mongodb.net/?appName=Cluster0";
+
+// Veritabanına bağlanıyoruz
+mongoose.connect(dbURI)
+  .then(() => console.log("MongoDB veritabanına başarıyla bağlanıldı! 🚀"))
+  .catch((err) => console.log("Veritabanı bağlantı hatası:", err));
+
 app.get('/', (req, res) => {
-  res.send('32Bit Garage API Sunucusu Başarıyla Çalışıyor! Isparta/SDÜ');
+  res.send('32Bit Garage API Sunucusu ve Veritabanı Çalışıyor! Isparta/SDÜ');
 });
 
-// Sunucuyu 5000 portunda ayağa kaldırıyoruz
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışmaya başladı...`);
-  console.log(`Test etmek için tarayıcıda şu adrese git: http://localhost:${PORT}`);
 });
